97502c2f4d54b97f454649b3b7d7a6e0c734857e
Vlad Rutskiy <vladrytskiy@gmail.com>
2023-11-01 16:31:14 +0200

    adding new js file with functions

---
+++ 04-04-functions.js
@@ -0,0 +1,16 @@
+function sum(a, b){
+    return a*b;
+}
+
+console.log(sum(10, 12)); //22?
+
+
+function digits(a, b, c){
+    return a*c-b;
+    }
+
+    console.log(digits(2, 3, 4)); // I hope it is 5?
+
+
+    const newDigits = digits(2, 6, 2);
+    console.log(newDigits);
