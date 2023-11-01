18796fd974991944f275111e1631f33d6d9758f2
Vlad Rutskiy <vladrytskiy@gmail.com>
2023-11-01 19:01:50 +0200

    learning classes

--- 05-05-02-objects.js
+++ 05-05-02-objects.js
@@ -5,9 +5,37 @@ setTimeout(() => {
 const vegitables_and_fruits = {
     name: "potato",
     isTasty: true,
-    "åß®√å¬©": false,
+    //"åß®√å¬©": false,
+    eatVegitable: function () {
+        console.log("I'm eating " + this.name);
+    }
 };
-vegitables_and_fruits.name = "tomato";
-vegitables_and_fruits.isTasty = false;
-console.log(vegitables_and_fruits["åß®√å¬©"]);
-console.log(vegitables_and_fruits);
+//vegitables_and_fruits.name = "tomato";
+//vegitables_and_fruits.isTasty = false;
+//console.log(vegitables_and_fruits["åß®√å¬©"]);
+//console.log(vegitables_and_fruits);
+vegitables_and_fruits.eatVegitable();
+
+
+
+class Food {
+    constructor(name, isTasty) {
+        this.name = name;
+        this.isTasty = isTasty;
+    }
+    eatVegitable() {
+        console.log("we are fond of " + this.name);
+    }
+}
+
+const myFood = new Food("cucumber", true);
+myFood.eatVegitable();
+console.log(myFood);
+
+const ourFood = new Food("banana", false);
+ourFood.eatVegitable(); 
+console.log(ourFood);
+
+const theirFood = new Food("pineapple", "sure");
+theirFood.eatVegitable();
+console.log (theirFood);
