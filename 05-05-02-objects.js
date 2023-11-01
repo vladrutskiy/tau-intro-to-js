b667dd672f751d155c41ac5b40a5a69ec0a78262
Vlad Rutskiy <vladrytskiy@gmail.com>
2023-11-01 17:40:20 +0200

    objects

--- 05-05-02-objects.js
+++ 05-05-02-objects.js
@@ -1,3 +1,13 @@
 setTimeout(() => {
     console.log("please wait")
 }, 4000);
+
+const vegitables_and_fruits = {
+    name: "potato",
+    isTasty: true,
+    "åß®√å¬©": false,
+};
+vegitables_and_fruits.name = "tomato";
+vegitables_and_fruits.isTasty = false;
+console.log(vegitables_and_fruits["åß®√å¬©"]);
+console.log(vegitables_and_fruits);
