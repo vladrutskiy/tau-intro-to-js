986eafd28c7ddbfafc78f5c143a9a295df6287fc
Vlad Rutskiy <vladrytskiy@gmail.com>
2023-11-08 19:32:24 +0200

    certificate

---
+++ 07-07.js
@@ -0,0 +1,36 @@
+//const cars = ["ferrari", "mercedes", "porshe", "lamborgini", "audi", "1", "2", "3"];
+
+//for(let p=0; p <cars.length; p++ ){
+//    console.log(cars[p]);
+//}
+
+
+//const numbers = [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7];
+ //for(let q=0; q < numbers.length; q++) {
+ //   console.log (numbers[q] / 5);
+// }
+
+// let password = "hello";
+// let userGuess = "";
+
+// while (userGuess !== password) {
+//    userGuess = prompt("Please enter your password");
+
+// }
+// alert("Success");
+
+
+
+
+let times = 2;
+let number = 5;
+let original = number;
+
+
+ do {
+    times = times + number;
+    number --
+
+ } while (number > 0);
+ console.log (original + " times is " + times);
+ 
