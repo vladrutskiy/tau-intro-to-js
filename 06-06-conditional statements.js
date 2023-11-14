a86455ca15dc3f058f451bba45ce9457ef15c9ad
Vlad Rutskiy <vladrytskiy@gmail.com>
2023-11-14 19:43:58 +0200

    if else

---
+++ 06-06-conditional statements.js
@@ -0,0 +1,20 @@
+var day="wed";
+switch(day){
+    case"mon": console.log("day is mon");
+    break;
+    case"tue": console.log("day si tue");
+    break;
+    case"wed": console.log("day is wed");
+    break;
+    case"sun": console.log("day is sun");
+}
+alert("hello today is  " + day);
+/*alert("today is …");
+
+var name = "John Doe";
+
+function myFunction() {
+  alert('Check ' + name);
+}
+<button onclick="myFunction()">Click here</button>;
+*/
