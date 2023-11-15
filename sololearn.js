2e8f157af7bdfde8277b52682a9ad220193c0144
Vlad Rutskiy <vladrytskiy@gmail.com>
2023-11-15 17:07:14 +0200

    Инструкция switch

--- sololearn.js
+++ sololearn.js
@@ -41,3 +41,87 @@ console.log(y);
 //console.log('I\'m happy');
 //console.log("She said \"Yes!\"");
 //console.log("One \nTwo \nThree");
+
+
+
+
+//Template Literals = Шаблонные литералы
+
+
+//console.log(`I'm happy, she said "Yes"!`);
+/*let msg = `Hey! Are you sololearning?
+Isn't it awesome?
+seems that the answer is "YES!"
+`;
+console.log(msg);
+*/
+
+/*let name = "John";
+let text = `Welcome, ${name}`;
+console.log(text);
+*/
+
+
+
+//Concatenation = Конкатенация 
+
+//console.log("Java" + 'Script');
+
+
+
+
+//Booleans
+
+//let score = 85;
+//console.log(score>99);
+/*
+равно ==
+не равно !=
+больше >
+меньше <
+больше или равно >=
+меньше или равно <=
+*/
+
+/*
+let purchase = 2556;
+let name = `Bob`
+
+if (purchase>=1500){
+    console.log(`Hello, dear ${name}! You have got a Discount! it is 10% because your purchaise is valid \$${purchase}`);
+    if (purchase>1551){
+        console.log(`Congratulations ${name}!
+        keep it up!`);
+    }
+}
+else if (purchase<=1000){
+    console.log(`Hello, dear ${name}! You haven't got a Discount! Because your purchaise is valid \$${purchase}`);
+}
+else if(purchase<=1499){
+    console.log(`Hello, dear ${name}! You have got a Discount! it is 5% because your purchaise is valid \$${purchase}`);
+}
+else {
+console.log(`Dear ${name}, it isn't your day, sorry.`);
+}
+*/
+
+
+
+
+//the switch statement = Инструкция switch
+
+let choice = "aj";
+
+switch(choice){
+  case "a"://match!
+    console.log("Sports");
+    break;
+  case "b":
+    console.log("Business");
+    break;
+  case "c":
+    console.log("Technology");
+    break;
+    default: 
+    console.log(`nothing to see here, yet`);
+}
